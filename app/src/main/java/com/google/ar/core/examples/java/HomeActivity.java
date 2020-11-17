dacd03e6065ecd148a3f614b6459bd746c7d9754
이관형 <rhksguda@naver.com>
2020-11-17 15:23:33 +0900

    레이아웃 아이디 정함

--- app/src/main/java/com/google/ar/core/examples/java/HomeActivity.java
+++ app/src/main/java/com/google/ar/core/examples/java/HomeActivity.java
@@ -3,14 +3,23 @@ package com.google.ar.core.examples.java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.google.ar.core.examples.java.augmentedimage.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    LinearLayout linearLayout_Home_Recommend,linearLayout_Home_Introduce,linearLayout_Home_ARroad,linearLayout_Home_ARgame;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //xml과 연결하기
+        linearLayout_Home_Recommend=findViewById(R.id.linearLayout_Home_Recommend);
+        linearLayout_Home_Introduce=findViewById(R.id.linearLayout_Home_Introduce);
+        linearLayout_Home_ARroad=findViewById(R.id.linearLayout_Home_ARroad);
+        linearLayout_Home_ARgame=findViewById(R.id.linearLayout_Home_ARgame);
     }
 }
